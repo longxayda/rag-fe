ea492da52c4efa470072f8e0f6fc9ed3ea91ee38
long <longxayda11@gmail.com>
2026-01-06 22:30:08 +0700

    Fix for demo

--- src/data/people.jsx
+++ src/data/people.jsx
@@ -1,5 +1,5 @@
 export const PEOPLE_DATA = [
-  { id: 1, name: 'Công tử Bạc Liêu', address: 'Phường Bạc Liêu', type: 'people', period: '1900-1972', description: 'Trần Trinh Huy', image: '👨' },
-  { id: 2, name: 'Tống Phước Phú', address: 'Phường Vĩnh Trạch', type: 'people', period: '1850-1920', description: 'Phú hào đất Bạc Liêu', image: '👤' },
+  // { id: 1, name: 'Công tử Bạc Liêu', address: 'Phường Bạc Liêu', type: 'people', period: '1900-1972', description: 'Trần Trinh Huy', image: '👨' },
+  // { id: 2, name: 'Tống Phước Phú', address: 'Phường Vĩnh Trạch', type: 'people', period: '1850-1920', description: 'Phú hào đất Bạc Liêu', image: '👤' },
 ];
 
